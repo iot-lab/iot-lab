e4a388b14b0e4855e9d1c1af0947b73ffa4b1065
Olivier Fambon <olivier.fambon@inria.fr>
2014-04-30 20:13:40 +0200

    added get_deployed_nodes.sh

---
+++ qualif/get_deployed_nodes.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -e
+
+exp_id=$1
+
+[ ! $exp_id ] && echo "usage: $0 <exp_id>" && false
+
+parse_json="$(dirname "$0")/parse_json.py"
+
+experiment-cli get -i $exp_id -p \
+| $parse_json "
+	[sys.stdout.write(node + '\n')
+	for node in x['deploymentresults']['0']]" \
+| grep -v -e None -e format
