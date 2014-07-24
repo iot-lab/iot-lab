c3da2c1ce24c6a113c3398d8d7616b66f6f31439
Olivier Fambon <olivier.fambon@inria.fr>
2014-07-24 20:32:48 +0200

    added multi-arch support, rephrased 'wait for ^C'

--- web-view/aggr.sh
+++ web-view/aggr.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-site=${site:-"grenoble"}
+site=${site:-grenoble}
+arch=${arch:-m3}
+
+m3="m3:at86rf231"
+wsn1="wsn430:cc1101"
+wsn2="wsn430:cc2420"
 
 targets_json() {
 	echo '{ "items": [ '
@@ -12,12 +17,20 @@ targets_json() {
 
 target() {
 	node_id=$1
-	echo '{ "archi": "m3:at86rf231", "site": "'$site'",
-		"network_address": "m3-'$node_id'" }'
+	echo '{ "archi": "'${!arch}'", "site": "'$site'",
+		"network_address": "'`node_type`'-'$node_id'" }'
+}
+
+node_type() {
+	cut -d : -f 1 <<< "${!arch}"
 }
 
 usage() {
 	echo "usage: `basename $0` <node id> [<node is> ...]"
+	echo
+	echo "       to specify alternative site or arch (m3|wsn1|wsn2):"
+	echo "       site=<site> arch=<arch> ./`basename $0` <node id> ..."
+	echo "       site=$site, arch=$arch"
 }
 
 case $1 in
@@ -25,7 +38,11 @@ case $1 in
 		usage && exit 0
 		;;
 esac
+
+[ ! `node_type` ] && echo "invalid arch: $arch" && exit 1
+
 ssh $site".iot-lab.info" "
 	./serial_aggregator.py 2>&1 <<< '`targets_json "$@"`' &
-	read; kill %1
+	cat > /dev/null
+	kill %1
 "
