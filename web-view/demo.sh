0b046f715f56d4449c023fcfb2c6f8102e3707e6
Olivier Fambon <olivier.fambon@inria.fr>
2014-07-26 13:58:21 +0200

    renamed speed => delay

--- web-view/demo.sh
+++ web-view/demo.sh
@@ -2,18 +2,18 @@
 
 ./httpd.py &
 firefox localhost:8000 &
-speed() { while read i; do echo $i; sleep $1; done; }
+delay() { while read i; do echo $i; sleep $1; done; }
 
 (
-	seq 36 69 | tac | speed .1;
-	seq 24 35 | tac | speed .2;
-	seq  1 23 | tac | speed .1;
-	seq 70 3 94  | speed .2 ;
-	seq 95 5 178 | speed .4;
-	seq 179 2 205 | speed .1;
-	seq 207 2 289 | speed .3;
-	seq 204 2 288 | tac | speed .2;
-	seq 290 358 | speed .1
+	seq 36 69 | tac | delay .1;
+	seq 24 35 | tac | delay .2;
+	seq  1 23 | tac | delay .1;
+	seq 70 3 94  | delay .2 ;
+	seq 95 5 178 | delay .4;
+	seq 179 2 205 | delay .1;
+	seq 207 2 289 | delay .3;
+	seq 204 2 288 | tac | delay .2;
+	seq 290 358 | delay .1
 ) | while read i; do
 	j=$((RANDOM*290/65535+70));
 	echo "{ $i: { style: 'splash' },
