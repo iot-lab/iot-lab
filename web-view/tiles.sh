95cabf787fc332dfa43a04e5310f1e03f64d8616
Olivier Fambon <olivier.fambon@inria.fr>
2014-07-26 19:18:47 +0200

    updated tiles.sh to use ./splash.sh

--- web-view/tiles.sh
+++ web-view/tiles.sh
@@ -2,19 +2,4 @@
 
 nodes=$*
 
-./aggr.sh $nodes \
-| awk -F '[-;]' '
-	/Peak/ {
-		node_id = $3
-		print node_id, ": { style: \"splash\" }"
-		fflush()
-	}
-' \
-| while true; do
-	while read -t 2 line; do
-		state="$state,$line"
-	done
-	[[ $? > 0 && $? < 128 ]] && exit 1
-	echo "{ ${state/,/} }" > user-state.json
-	state=
-done 
+./aggr.sh $nodes | awk -F '[-;]' '/Peak/ { print $3 }' | ./splash.sh
