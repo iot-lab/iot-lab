ee4701c36827736ed9b9fc688b2a0f2edc35b41a
Olivier Fambon <olivier.fambon@inria.fr>
2014-07-27 23:04:08 +0200

    updated demo to use splash.sh

--- web-view/demo.sh
+++ web-view/demo.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-./httpd.py &
+./httpd.py &> /dev/null &
 firefox localhost:8000 &
+
 delay() { while read i; do echo $i; sleep $1; done; }
+alter() { awk 'NR % 2 { print; next } { print $1 " circle green" }'; }
 
 (
 	seq 36 69 | tac | delay .1;
-	seq 24 35 | tac | delay .2;
-	seq  1 23 | tac | delay .1;
-	seq 70 3 94  | delay .2 ;
+	seq 24 35 | tac | alter | delay .1;
+	seq  1 23 | tac | delay .2;
+	seq 70 3 94  | alter | delay .03;
 	seq 95 5 178 | delay .4;
 	seq 179 2 205 | delay .1;
 	seq 207 2 289 | delay .3;
 	seq 204 2 288 | tac | delay .2;
-	seq 290 358 | delay .1
-) | while read i; do
-	j=$((RANDOM*290/65535+70));
-	echo "{ $i: { style: 'splash' },
-		$j: { style: 'circle green' }
-	}" > user-state.json
-done
+	seq 290 358 | alter | delay .1;
+	sleep 1.5;
+) | ./splash.sh
