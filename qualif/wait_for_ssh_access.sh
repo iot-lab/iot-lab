9a89b6125de21df6bde29d093a91c0ac97245087
Olivier Fambon <olivier.fambon@inria.fr>
2014-09-12 10:01:44 +0200

    cleanup tmp files at exit, in case

--- qualif/wait_for_ssh_access.sh
+++ qualif/wait_for_ssh_access.sh
@@ -10,17 +10,20 @@ NODES_LIST=$(experiment-cli get -i $exp_id -p | ./parse_json.py "
 	' '.join([str('node-'+node)
 	for node in x['deploymentresults']['0']])")
 
+tmpfiles=/tmp/$$.failed
+trap "rm -f $tmpfiles.*" EXIT
+
 max_time=$(date -d "now + 5 min" +%s)
 nodes=$NODES_LIST
 while [ "$nodes" ]; do
 	for node in $nodes
 	do 
 		ssh $node -o ConnectTimeout=5 &>/dev/null \
-			date || echo $node > /tmp/$$.failed.$node &
+			date || echo $node > $tmpfiles.$node &
 		[ $[ i = (i+1) % 10 ] = 0 ] && sleep 1
 	done
 	wait
-	nodes=$(touch /tmp/$$.failed; cat /tmp/$$.failed*; \rm /tmp/$$.failed*)
+	nodes=$(touch $tmpfiles; cat $tmpfiles*; \rm $tmpfiles*)
 	if [[ $(date +%s) > $max_time ]]; then
 		echo "$nodes" | sort -t - -n -k3
 		exit 2
