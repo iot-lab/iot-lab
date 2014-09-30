88c54d0b3e79550cf98fd4ef7157e39cf2e16753
Olivier Fambon <olivier.fambon@inria.fr>
2014-09-30 16:52:08 +0200

    added MagPeak detector (mapped to 'circle')

--- web-view/tiles.sh
+++ web-view/tiles.sh
@@ -4,5 +4,8 @@ node_set_name=$1
 nodes=$(grep "\"$node_set_name\"" nodes-sets.json | sed 's/.*: "//; s/".*//')
 [ ! "$nodes" ] && echo "usage: $0 <node set name>" && exit 1
 
-./aggr.sh $nodes | awk -F '[-;]' '/AccPeak/ { print $3; fflush() }' \
+./aggr.sh $nodes | awk -F '[-;]' '
+/AccPeak/ { print $3, "splash"; fflush() }
+/MagPeak/ { print $3, "circle"; fflush() }
+' \
 | tee /dev/stderr | ./splash.sh --queue-size 10
