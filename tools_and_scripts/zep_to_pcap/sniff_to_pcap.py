76f82da989f23b5f05a6b712af5dc2cb10f9bc54
Gaëtan Harter <gaetan.harter@inria.fr>
2014-12-09 17:38:02 +0100

    Implement Zep to Pcap as a class.

--- tools_and_scripts/zep_to_pcap/sniff_to_pcap.py
+++ tools_and_scripts/zep_to_pcap/sniff_to_pcap.py
@@ -9,105 +9,154 @@
 #      Generate-a-quick-and-easy-custom-pcap-file-using-P
 
 import sys
-import binascii
-
-PCAP_GLOBAL_HEADER = (
-    'D4 C3 B2 A1'
-    '02 00'         # File format major revision (i.e. pcap <2>.4)
-    '04 00'         # File format minor revision (i.e. pcap 2.<4>)
-    '00 00 00 00'   # GMT to local correction: 0 if timestamps are UTC
-    '00 00 00 00'   # accuracy of timestamps -> set it to 0
-    'FF FF 00 00'   # packet capture limit -> typically 65535
-    '01 00 00 00'   # Ethernet
-)
-
-# pcap packet header that must preface every packet
-PCAP_PACKET_HEADER = (
-    'AA 77 9F 47'  # timestamp seconds  # TODO
-    '90 A2 04 00'  # timestamps microseconds TODO
-    'XX XX XX XX'  # Frame size (little endian) // number of octets in file
-    'YY YY YY YY'  # Frame size (little endian) // real size not truncated
-)
-
-ETH_HEADER = (
-    '00 00 00 00 00 00'  # Source Mac
-    '00 00 00 00 00 00'  # Dest Mac
-    '08 00'              # Protocol (0x0800 = IP)
-)
-
-IP_HEADER = (
-    '45'           # IP version and header length (multiples of 4 bytes)
-    '00'
-    'XX XX'        # Length - Will be calculated and replaced later
-    '00 00'
-    '40 00 40'
-    '11'           # Protocol (0x11 = UDP)
-    'YY YY'        # Checksum - Will be calculated and replaced later
-    '7F 00 00 01'  # Source IP (Default: 127.0.0.1)
-    '7F 00 00 01'  # Dest IP   (Default: 127.0.0.1)
-)
-
-UDP_HEADER = (
-    '45 5a'  # SrcPort: use ZepPort also but not required
-    '45 5a'  # DstPort: ZepPort 17754 in hexa
-    'YY YY'  # Length - Will be calculated and replaced later
-    '00 00'  # checksum
-)
+import datetime
+import calendar
+import struct
 
 
-def get_byte_len(byte_str):
-    """ Return the binascii string length """
-    return len(''.join(byte_str.split())) / 2
+class ZepPcap(object):  # pylint:disable=too-few-public-methods
+    """ Zep to Pcap converter
+    On `write` encapsulate the message as a zep packet in `outfile` pcap format
+    """
+    zep_port = 17754
+    # PCAP headers as native endian
+    pcap_global_hdr = struct.pack(
+        '=LHHLLLL',
+        0xa1b23c4d,    # Pcap header Little Endian
+        2,             # File format major revision (i.e. pcap <2>.4)
+        4,             # File format minor revision (i.e. pcap 2.<4>)
+        0,             # GMT to local correction: 0 if timestamps are UTC
+        0,             # accuracy of timestamps -> set it to 0
+        0xffff,        # packet capture limit -> typically 65535
+        1,             # Link: Ethernet
+    )
 
+    # Network headers as network endian
+    eth_hdr = struct.pack('!3H3HH',
+                          0, 0, 0,  # dst mac addr
+                          0, 0, 0,  # src mac addr
+                          0x0800)   # Protocol: (0x0800 == IP)
 
-def to_pcap(zep_message):
-    """ generate pcap from binary message """
+    def __init__(self, outfile):
+        self.out = outfile
+        # Write global header
+        self.out.write(self.pcap_global_hdr)
+        self.out.flush()
 
-    udp_len = len(zep_message) + get_byte_len(UDP_HEADER)
-    udp_h = UDP_HEADER.replace('YY YY', '%04x' % udp_len)
+    def write(self, packet):
+        """ Save packet in pcap outfile """
+        date = datetime.datetime.utcnow()
+        timestamp = (calendar.timegm(date.utctimetuple()), date.microsecond)
 
-    ip_len = udp_len + get_byte_len(IP_HEADER)
-    ip_h = IP_HEADER.replace('XX XX', '%04x' % ip_len)
-    checksum = ip_checksum(ip_h.replace('YY YY', '00 00'))
-    ip_h = ip_h.replace('YY YY', "%04x" % checksum)
+        # Calculate all headers
+        length = len(packet)
 
-    pcap_len = ip_len + get_byte_len(ETH_HEADER)
-    hex_str = "%08x" % pcap_len
-    # TODO check this with endiannes
-    reverse_hex_str = hex_str[6:] + hex_str[4:6] + hex_str[2:4] + hex_str[:2]
-    pcap_h = PCAP_PACKET_HEADER.replace('XX XX XX XX', reverse_hex_str)
-    pcap_h = pcap_h.replace('YY YY YY YY', reverse_hex_str)
+        udp_hdr = self._udp_header(length)
+        length += len(udp_hdr)
 
-    bytestring = to_bin(pcap_h + ETH_HEADER + ip_h + udp_h) + zep_message
-    return bytestring
+        ip_hdr = self._ip_header(length)
+        length += len(ip_hdr)
 
+        eth_hdr = self._eth_header(length)
+        length += len(eth_hdr)
 
-def split_n(str1, num):
-    """ Splits the string into a list of tokens every num characters """
-    return [str1[start:start+num] for start in range(0, len(str1), num)]
+        pcap_hdr = self._pcap_header(length, timestamp[0], timestamp[1])
+        length += len(pcap_hdr)
 
+        # Actually write the data
+        self.out.write(pcap_hdr)
+        self.out.write(eth_hdr)
+        self.out.write(ip_hdr)
+        self.out.write(udp_hdr)
+        self.out.write(packet)
+        self.out.flush()
 
-def ip_checksum(iph):
-    """ Calculates and returns the IP checksum based on the given IP Header """
-    # split into bytes
-    words = split_n(''.join(iph.split()), 4)
-    csum = 0
-    for word in words:
-        csum += int(word, base=16)
-    csum += (csum >> 16)
-    csum = csum & 0xFFFF ^ 0xFFFF
+    def _udp_header(self, pkt_len):
+        """ Get UDP Header
 
-    return csum
+        2B - src_port: ZEP_PORT also but not required
+        2B - dst_port: ZEP_PORT == 17754
+        2B - length:   header + packet length
+        2B - checksum: Disable == 0
 
+        """
+        hdr_struct = struct.Struct('!HHHH')
+        udp_len = hdr_struct.size + pkt_len
+        udp_hdr = hdr_struct.pack(self.zep_port, self.zep_port, udp_len, 0)
+        return udp_hdr
 
-def to_bin(byte_string):
-    """ Remove all whitespaces from binascii and return binary """
-    return binascii.a2b_hex(''.join(byte_string.split()))
+    def _ip_header(self, pkt_len):
+        """ Get the IP Header
+
+        1B - Version | IHL:           0x45: [4 | 5] : [4b | 4b]
+        1B - Type of Service:         0
+        2B - Length:                  pkt_len + Header length
+        2B - Identification:          0
+        2B - Flags | Fragment offset: 0x4000: [2 | 0] : [3b | 13b]
+        1B - TTL:                     0xff
+        1B - Protocol:                0x11 UDP
+        2B - Checksum:                calculated
+        4B - Source Address:          0x7F000001 (127.0.0.1)
+        4B - Destination Address:     0x7F000001 (127.0.0.1)
+
+        """
+        hdr_struct = struct.Struct('!BBHHHBBHLL')
+        ip_len = hdr_struct.size + pkt_len
+
+        # generate header with checksum == 0 to calculate checksum
+        checksum = 0
+        ip_hdr_csum = hdr_struct.pack(0x45, 0, ip_len, 0, 0x4000, 0xff, 0x11,
+                                      checksum, 0x7F000001, 0x7F000001)
+        checksum = self._ip_checksum(ip_hdr_csum)
+
+        # Generate header with correct checksum
+        ip_hdr = hdr_struct.pack(0x45, 0, ip_len, 0, 0x4000, 0xff, 0x11,
+                                 checksum, 0x7F000001, 0x7F000001)
+        return ip_hdr
+
+    def _eth_header(self, pkt_len):  # pylint:disable=unused-argument
+        """ Return a static empty ethernet header
+
+        6B - dst mac addr: 0
+        6B - src mac addr: 0
+        2B - protocol:     0x0800 (IP)
+        """
+        return self.eth_hdr
+
+    @staticmethod
+    def _pcap_header(pkt_len, t_s, t_us):
+        """ Get the PCAP Header
+
+        4B - Timestamp seconds:      current time
+        4B - Timestamp microseconds: current time
+        4B - Number of octet saved:  pkt_len
+        4B - Actual lengt of packet: pkt_len
+        """
+
+        hdr_struct = struct.Struct('=LLLL')
+        pcap_len = pkt_len
+        pcap_hdr = hdr_struct.pack(t_s, t_us, pcap_len, pcap_len)
+        return pcap_hdr
+
+    @staticmethod
+    def _ip_checksum(hdr):
+        """ Calculate the ip checksum for given header """
+        assert 0 == (len(hdr) % 2)  # hdr has even length
+        word_pack = struct.Struct('!H')
+
+        # Sum all 16bits
+        hdr_split = (hdr[i:i+2] for i in range(0, len(hdr), 2))
+        csum = sum((word_pack.unpack(word)[0] for word in hdr_split))
+
+        # Reduce to 16b and save the one complement
+        checksum = (csum + (csum >> 16)) & 0xFFFF ^ 0xFFFF
+        return checksum
 
 
 def main():
     """ Main function """
 
+    import binascii
     zep_message_str = (
         '45 58 02 01'   # Base Zep header
         '0B 00 01 00 ff'   # chan | dev_id | dev_id| LQI/CRC_MODE |  LQI
@@ -124,20 +173,17 @@ def main():
         '41 42 43'      # Data
         'FF FF'         # CRC)
     )
-    zep_message = to_bin(zep_message_str)
+    zep_message = binascii.a2b_hex(''.join(zep_message_str.split()))
 
-    pcap_hdr = to_bin(PCAP_GLOBAL_HEADER)
-    pcap_msg = to_pcap(zep_message)
+    out_file = sys.argv[1]
+    with open(out_file, 'w') as pcap_file:
+        zep_pcap = ZepPcap(pcap_file)
 
-    try:
-        out_file = sys.argv[1]
-        with open(out_file, 'w') as pcap_file:
-            pcap_file.write(pcap_hdr)
-            pcap_file.write(pcap_msg)
-    except IndexError:
-        out = binascii.b2a_hex(pcap_hdr)
-        out += binascii.b2a_hex(pcap_msg)
-        print out
+        zep_pcap.write(zep_message)
+        zep_pcap.write(zep_message)
+        zep_pcap.write(zep_message)
+        zep_pcap.write(zep_message)
+        zep_pcap.write(zep_message)
 
 
 if __name__ == '__main__':
