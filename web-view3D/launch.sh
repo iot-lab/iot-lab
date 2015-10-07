e78fe835e29b44d1532e2728022e5f640be5e04e
Julien Vandaele <julien.vandaele@inria.fr>
2015-10-07 12:49:52 +0200

    improve launching script Opens commands in tabs, retitled.

--- web-view3D/launch.sh
+++ web-view3D/launch.sh
@@ -6,7 +6,6 @@ SITE=lille
 # experiment-cli submit ...
 # experiment-cli wait ...
 
-gnome-terminal -e "socat tcp-listen:9000,fork,reuseaddr exec:'ssh $USER@$SITE.iot-lab.info \"serial_aggregator\"'" &
-gnome-terminal --working-directory /home/fun/iot-lab/web-view3D -e "nodejs app.js && bash" &
-sleep 1
-firefox localhost:3000 &
+gnome-terminal --tab -t tunnel -e "socat tcp-listen:9000,fork,reuseaddr exec:'ssh $USER@$SITE.iot-lab.info \"serial_aggregator\"'" --tab -t nodejs --profile demo --working-directory /home/fun/iot-lab/web-view3D -e "nodejs app.js && bash" &
+sleep 2
+firefox localhost:3000
