20c54039f4672151fb3ba95623fb775f959ca638
Saint-Marcel Frederic <frederic.saint-marcel@inria.fr>
2019-03-20 08:18:10 +0100

    remove obsolete packer virtual machine contents

--- packer-template/ubuntu-14.04/scripts/locale.sh
+++
@@ -1,4 +0,0 @@
-apt-get -y install language-pack-fr
-sed -i 's/XKBLAYOUT=.*/XKBLAYOUT="us,fr"/g' /etc/default/keyboard
-sed -i 's/XKBOPTIONS=.*/XKBOPTIONS="grp:ctrl_shift_toggle"/g' /etc/default/keyboard
-sed -i 's/show-indicators=.*/show-indicators=power;~session;~a11y;~power;/g' /etc/lightdm/lightdm-gtk-greeter.conf
